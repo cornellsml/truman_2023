8551c38cf4a57edfb5fbdba168f2b5b7da8ad630
Winice Hui <winice.hui@gmail.com>
2024-03-04 17:22:51 -0500

    Remove Adhit's scripts

--- scripts/test.py
+++
@@ -1,6 +0,0 @@
-import sys 
-# Takes first name and last name via command  
-# line arguments and then display them 
-logger.info("Output from Python") 
-logger.info("First name: " + sys.argv[1]) 
-logger.info("Last name: " + sys.argv[2]) 
